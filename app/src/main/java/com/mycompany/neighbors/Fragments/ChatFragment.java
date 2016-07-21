32c884de81c9e83dc2b1455030fabe2a0a5bac89
Robbie08 <Roberinho08@gmail.com>
2016-07-20 20:06:16 -0700

    Revert "Chat Fragment updates"
    This reverts commit 7ee7994ae28b3ea85260d334b0fc22796b3ffe6b.

--- app/src/main/java/com/mycompany/neighbors/Fragments/ChatFragment.java
+++ app/src/main/java/com/mycompany/neighbors/Fragments/ChatFragment.java
@@ -1,25 +1,7 @@
 package com.mycompany.neighbors.Fragments;
 
-import android.os.Bundle;
-import android.support.v4.app.ListFragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.mycompany.neighbors.R;
-
 /**
  * Created by joshua on 5/25/2016.
  */
-public class ChatFragment extends ListFragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View withPussy = inflater.inflate(R.layout.fragment_chat,container,false);
-        //lol....cute
-        TextView tvMessageBox = (TextView) withPussy.findViewById(R.id.tvMessageBox);
-
-        return withPussy;
-    }
+public class ChatFragment {
 }
